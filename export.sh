567b4712ad55147ff649ec30b7b81f03de29831f
eric <ericzhao96@hotmail.com>
2022-03-16 22:44:39 +0800

    feat: support amd64 and arm64

--- export.sh
+++ export.sh
@@ -9,4 +9,6 @@ mkdir -p output
 cp -r ./static/icons ./output/icons
 cp ./static/workflow/* ./output/
 
-go build -o output/devdocs_alfred
+GOOS=darwin GOARCH=amd64 go build -o output/devdocs_alfred_amd64
+GOOS=darwin GOARCH=arm64 go build -o output/devdocs_alfred_arm64
+
